0bf367d45bea75b5ea0d7f229033a541f570fb56
snippet-softwares <snippetsoftwaresolutions@gmail.com>
2021-09-06 17:29:06 +0300

    Changed to Bootstrap 4.4.1 and linked product to product details page onclick event

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ProductService} from "../../services/product.service";
 import {count} from "rxjs/operators";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -11,7 +12,8 @@ export class HomeComponent implements OnInit {
 
   products: any[] = [];
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.productService.getAllProducts().subscribe((prods: {count: Number, products: any[]}) => {
@@ -20,4 +22,7 @@ export class HomeComponent implements OnInit {
     });
   }
 
+  selectProduct(id: Number) {
+    this.router.navigate(['/product', id]).then();
+  }
 }
